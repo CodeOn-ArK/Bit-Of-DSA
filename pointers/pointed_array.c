c5625039f4e6c5a821cd8c9a666fd82bef2bd466
CodeOn-ArK <aarya.codeeka123@gmail.com>
2021-09-17 11:01:50 +0530

    2D array using pointer

--- pointers/pointed_array.c
+++ pointers/pointed_array.c
@@ -2,6 +2,8 @@
 #include<stdint.h>
 #include<stdlib.h>
 
+void display_matrix(uint32_t **);
+
 int main(){
   uint32_t **ptr2ptr = NULL, *ptr = NULL, **head = NULL;
 
@@ -18,24 +20,46 @@ int main(){
  i=0;
  ptr2ptr = head;
 
+ printf("         (ptr2ptr)   *(ptr2ptr)    **(ptr2ptr)\n\r");
  while(i < 3){
    j=0;
    while(j < 3){
-      printf("%d, %x, %x\t",j, (*ptr2ptr), ptr2ptr);
-      scanf("%d", (**ptr2ptr));
-      (*ptr2ptr)++;
+      printf("j=%d, i=%d, %x,\t %x,\t %d\t\t",j,i, (ptr2ptr) + i, *(ptr2ptr+i) +j, *(*(ptr2ptr+i) + j) );
+      scanf("%d", (*(ptr2ptr+i) + j));
       j++;
    }
 
-   ptr2ptr++;
    i++;
-
    printf("\n\r");
  }
 
+ PRIntf("\n\r");
+ i=0;
 
+ while(i < 3){
+   j=0;
+   while(j < 3){
+      printf("j=%d, i=%d, %x,\t %x,\t %d\n",j,i, (ptr2ptr) + i, *(ptr2ptr+i) +j, *(*(ptr2ptr+i) + j) );
+      j++;
+   }
+
+   i++;
+   printf("\n\r");
+ }
+
+ display_matrix(ptr2ptr);
  return 0;
-
 }
 
 
+void display_matrix(uint32_t **ptr){
+  int i=0,j=0;
+  while(i < 3){
+    j=0;
+    while(j < 3){
+      printf("%d\t", *(*(ptr + i)+ j++) );
+    }
+    i++;
+    printf("\n\r");
+  }
+}
